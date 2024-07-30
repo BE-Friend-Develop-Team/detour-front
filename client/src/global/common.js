b99422e9b6f551605c007a8452afec1a8fd304a7
LeeNaYoung240 <ajd0927@naver.com>
2024-07-30 20:23:41 +0900

    ✨ feat : global 및 component style 추가
    Co-authored-by: eunsaemsaem <sweeet0308@gmail.com>
    Co-authored-by: wondo8449 <wondo8448@naver.com>
    Co-authored-by: LeeChangHyeong <chxngbro@gmail.com>
    Co-authored-by: sihyun615 <sihyun615@naver.com>

---
+++ client/src/global/common.js
@@ -0,0 +1,20 @@
+import { css } from "styled-components";
+
+export const flexCenter = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const flexCenterColumn = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const flexColumn = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
---
+++ client/src/global/theme.js
@@ -0,0 +1,56 @@
+const theme = {};
+
+theme.PALETTE = {
+    primary: {
+        main: "#FFE8A3",
+        sub: "#FFDDDD",
+    },
+    secondary: {
+        main: "#9747FF",
+        sub: "#FDDDB3",
+    },
+    white: "#FFFFFF",
+    gray: {
+        100: "#F0ECEC",
+        200: "#AEA8B2",
+        300: "#B3B3B3",
+    },
+    error: "#EB6144",
+    background: {
+        white: "#FFFFFF",
+        main: "#FFE8A3",
+        yellow: "#FFFAEA",
+        kakao: "#F9E000",
+        gray: "#D9D9D9",
+        black: "#1E1E1E",
+    },
+    black: "#332E21",
+};
+
+theme.FONT_SIZE = {
+    h1: "55px",
+    h2: "40px",
+    h3: "30px",
+    h4: "21px",
+    h5: "18px",
+    h6: "16px",
+    h7: "12px",
+};
+
+theme.FONT_WEIGHT = {
+    thin: "100",
+    regular: "400",
+    blod: "800",
+};
+
+theme.FONT_LINE = {
+    h1: "75px",
+    h2: "55px",
+    h3: "41px",
+    h4: "29px",
+    h5: "25px",
+    h6: "22px",
+    h7: "17px",
+};
+
+export default theme;
